28305843a7705dde3314eb51894846a8090b9a40
wayne2604 <rmanunag308@gmail.com>
2026-05-18 11:24:07 +0800

    Rename repo and add Kotlin placeholder for language detection

---
+++ app/src/main/java/com/wayne2604/genmind/MainActivity.kt
@@ -0,0 +1,9 @@
+package com.wayne2604.genmind
+
+/**
+ * Placeholder for language detection.
+ * Built with Kotlin & Jetpack Compose.
+ */
+class MainActivity {
+    // Gen-MIND: Mendelian Inheritance Learning App
+}
